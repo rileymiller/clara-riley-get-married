138989f4599b2a2713076d16ef18e34d398b7719
Riley Miller <rmllr000@gmail.com>
2021-04-28 13:43:21 -0600

    feat: add radio buttons and plus one logic

--- src/components/rsvp/RSVPForm.tsx
+++ src/components/rsvp/RSVPForm.tsx
@@ -1,5 +1,5 @@
 import { lighten, saturate } from 'polished';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 
 import { css } from '@emotion/react';
@@ -8,10 +8,11 @@ import { colors } from '../../styles/colors';
 import config from '../../website-config';
 import * as rsvp from '../../rsvp/rsvp.json';
 // import { gu } from 'date-fns/locale';
+import { EmotionCanvasTheme } from '@workday/canvas-kit-react-common';
 import { Radio, RadioGroup } from '@workday/canvas-kit-react-radio';
 import FormField from '@workday/canvas-kit-react-form-field';
 
-type RSVPGuest = {
+type RSVPGuests = {
   names: string[]
   accessCode: string
   plusOne: boolean
@@ -20,7 +21,7 @@ type RSVPGuest = {
 export const RSVPForm: React.FC = () => {
   const [accessCode, setAccessCode] = useState('');
   const [guests] = useState(rsvp.guests);
-  const [rsvpGuest, setRSVPGuest] = useState<RSVPGuest>();
+  const [rsvpGuests, setRSVPGuests] = useState<RSVPGuests>();
 
   const getGuestInviteFromAccessCode = (accessCode: string) =>
     guests.filter(invite => invite.accessCode === accessCode);
@@ -30,8 +31,7 @@ export const RSVPForm: React.FC = () => {
     e.preventDefault();
     const guest = getGuestInviteFromAccessCode(accessCode)[0];
     if (guest) {
-      // alert(JSON.stringify(guest));
-      setRSVPGuest(guest);
+      setRSVPGuests(guest);
     } else {
       alert(`Not found`);
     }
@@ -39,7 +39,7 @@ export const RSVPForm: React.FC = () => {
 
   return (
     <>
-      {!rsvpGuest && <AccessCodeNote>Enter the Access Code on the Back of Your RSVP Card</AccessCodeNote>}
+      {!rsvpGuests && <AccessCodeNote>Enter the Access Code on the Back of Your RSVP Card</AccessCodeNote>}
       <div css={css`
         display:flex;
         flex-direction: column;
@@ -51,7 +51,7 @@ export const RSVPForm: React.FC = () => {
         }
       `}
       >
-        {!rsvpGuest &&
+        {!rsvpGuests &&
           <form
             noValidate
             css={RSVPFormStyles}
@@ -61,8 +61,6 @@ export const RSVPForm: React.FC = () => {
               <RSVPEmail
                 className="RSVP-email"
                 type="email"
-                // name={config.mailchimpEmailFieldName}
-                // id={config.mailchimpEmailFieldName}
                 placeholder="Access Code (e.g. D49)"
                 onChange={e => setAccessCode(e.currentTarget.value)}
               />
@@ -73,79 +71,179 @@ export const RSVPForm: React.FC = () => {
 
           </form>}
 
-        {rsvpGuest &&
-          <section css={
-            css`
-              color: black;
-              display:flex;
-              flex-direction: column;
-              justify-content:center;
-              align-items: center;
+        {rsvpGuests?.names.length && <VerifiedGuestForm names={rsvpGuests.names} wasAllotedPlusOne={rsvpGuests.plusOne} />}
 
-              width:100%;
-
-              /* @media (max-width: 500px) {
-                width: 100%;
-              } */
-            `
-          }
-          >
-            {rsvpGuest.names.map(guest => {
-              return (
-                <GuestRSVPControl key={guest} guest={guest} />
-              );
-            })}
-            <p css={css`font-size: 1.4rem !important;margin: 2rem!important;`}>Please note any dietary restrictions below.</p>
-            <textarea
-              css={css`
-              width: 70%;
-              font-size: 1.4rem;
-              line-height: 1em;
-              min-height: 8em;
-              padding: 1.5rem;
-              border-radius: .4rem;
-              resize: none;
-              outline: ${lighten('-0.1', colors.royalty.blue)};
-              :focus {
-                border-color: ${lighten('-0.1', colors.royalty.blue)};
-                background: #e7f1fe;
-                transition: background 0.35s ease-in-out;
-              }
-              @media (max-width: 500px) {
-                width: 100%;
-              }
-            `} rows={4} />
-            <RSVPFormButton css={css`margin-top: 1.4rem;`} type="submit" onClick={() => console.log(`bend the knee`)}>
-              <span>Submit</span>
-            </RSVPFormButton>
-          </section>}
       </div>
     </>
   );
 };
 
+type RSVPAnswer = 'accepts' | 'declines';
+
+type GuestResponse = {
+  name: string
+  rsvp: RSVPAnswer
+  onChange: (rsvp: RSVPAnswer) => void
+};
+
+const initializeRsvpGuestToGuestResponse = (names: string[]): GuestResponse[] => names.map(name => {
+  const [rsvpChoice, setRSVP] = useState<RSVPAnswer>('accepts');
+
+  const onChange = (value: RSVPAnswer) => {
+    setRSVP(value);
+  };
+
+  return {
+    name,
+    rsvp: rsvpChoice,
+    onChange,
+  };
+});
+
+const VerifiedGuestForm = ({ names, wasAllotedPlusOne }: { names: string[], wasAllotedPlusOne?: boolean }) => {
+  const guests = initializeRsvpGuestToGuestResponse(names);
+  const [dietaryRestrictions, setDietaryRestrictions] = useState('');
+  const [plusOne, setPlusOne] = useState('');
+  const [isBringingPlusOne, setIsBringingPlusOne] = useState('no');
+  const onSubmit = () => {
+    console.log(`guests: ${JSON.stringify(guests)} \nDietary Restrictions: ${dietaryRestrictions}\n Plus One: ${plusOne}`);
+  };
+
+  const isComing = () => Boolean(guests.filter(guest => guest.rsvp === 'accepts').length);
+
+  return (
+    <section css={
+      css`
+        color: black;
+        display:flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items: center;
+
+        width:100%;
+      `
+    }
+    >
+      {guests.map(guest => {
+        return (
+          <GuestRSVPControl key={guest.name} name={guest.name} rsvp={guest.rsvp} onChange={guest.onChange} />
+        );
+      })}
+      {(wasAllotedPlusOne && isComing()) && <PlusOneControl isBringingPlusOne={isBringingPlusOne} setPlusOne={setPlusOne} setIsBringingPlusOne={setIsBringingPlusOne} />}
+      {isComing() &&
+        <>
+          <p css={css`font-size: 1.4rem !important;margin: 2rem!important;`}>Please note any dietary restrictions below.</p>
+          <textarea
+            css={css`
+        width: 70%;
+        font-size: 1.4rem;
+        line-height: 1em;
+        min-height: 8em;
+        padding: 1.5rem;
+        background: ${colors.royalty.ivory};
+        border: solid .2rem ${colors.royalty.blue};
+        border-radius: .4rem;
+        resize: none;
+        outline: ${lighten('-0.1', colors.royalty.blue)};
+        :focus, :active {
+          border-color: ${lighten('-0.1', colors.royalty.blue)};
+          background: #e7f1fe;
+          transition: background 0.35s ease-in-out;
+        }
+        @media (max-width: 500px) {
+          width: 100%;
+        }
+      `} rows={4}
+            value={dietaryRestrictions}
+            onChange={e => setDietaryRestrictions(e.target.value)}
+          />
+        </>}
+      <RSVPFormButton css={css`margin-top: 1.4rem;`} type="submit" onClick={onSubmit}>
+        <span>Submit</span>
+      </RSVPFormButton>
+    </section>
+  );
+};
+
+type PlusOneOption = 'yes' | 'no';
+type PlusOneControlProps = {
+  // plusOne: string
+  setPlusOne: (person: string) => void
+  isBringingPlusOne: string
+  setIsBringingPlusOne: (choice: PlusOneOption) => void
+};
+
+const PlusOneControl = (props: PlusOneControlProps) => {
+  return (
+    <>
+      <h3
+        // key={guest}
+        css={css`
+        font-weight: bold;
+        `}
+      >
+        Will you be bringing a plus one?
+      </h3>
+      <div css={[RadioLabelStyles]}>
+        <RadioGroup value={props.isBringingPlusOne} onChange={value => props.setIsBringingPlusOne(value as PlusOneOption)}>
+          <Radio key="yes" style={{ fontFamily: 'Andina Regular' }} id="1" value="yes" label="Yes" disabled={false} />
+          <Radio key="no" style={{ fontFamily: 'Andina Regular' }} id="2" value="no" label="No" disabled={false} />
+        </RadioGroup>
+      </div>
+      {
+        props.isBringingPlusOne === 'yes' &&
+        <FormGroup className="form-group">
+          <RSVPEmail
+            className="RSVP-plusOne"
+            type="text"
+            placeholder="Name of Plus One"
+            disabled={props.isBringingPlusOne !== 'yes'}
+            onChange={e => props.setPlusOne(e.currentTarget.value)}
+          />
+        </FormGroup>
+      }
+    </>
+  );
+};
+
 type GuestRSVPControlProps = {
-  guest: string
+  name: string
+  rsvp: RSVPAnswer
+  onChange: (rsvp: RSVPAnswer) => void
 };
 
 const GuestRSVPControl = (props: GuestRSVPControlProps) => {
-  const [value, setValue] = useState('accepts');
+  // const [value, setValue] = useState('accepts');
+  const onChange = (value: string | number) => {
+    // setValue(value as string);
+    props.onChange(value as RSVPAnswer);
+  };
 
   return (
     <div css={css`display:flex; justify-content: center;flex-direction:column; align-items: center; margin-bottom: 0.4rem;`}>
       <h3
         // key={guest}
-        css={css`font-weight: bold;`}
+        css={css`
+        font-weight: bold;
+        `}
       >
-        {props.guest}
+        {props.name}
       </h3>
-      <RadioGroup value={value} onChange={value => setValue(value as string)}>
-        <Radio key={`${props.guest}-yes`} id="1" value="accepts" label="Accepts with Pleasure" disabled={false} onChange={() => console.log('yes')} />
-        <Radio key={`${props.guest}-no`} id="2" value="declines" label="Declines with Regret" disabled={false} onChange={() => console.log('no')} />
-      </RadioGroup>
+      <div css={[RadioLabelStyles]}>
+        <RadioGroup value={props.rsvp} onChange={onChange}>
+          <Radio key={`${props.name}-yes`} style={{ fontFamily: 'Andina Regular' }} id="1" value="accepts" label="Accepts with Pleasure" disabled={false} />
+          <Radio key={`${props.name}-no`} style={{ fontFamily: 'Andina Regular' }} id="2" value="declines" label="Declines with Regret" disabled={false} />
+        </RadioGroup>
+      </div>
     </div>
   );
 };
+
+const RadioLabelStyles = css`
+    label {
+    font-family: 'Raleway';
+  }
+`;
 
 const AccessCodeNote = styled.p`
   font-size: 2rem;
