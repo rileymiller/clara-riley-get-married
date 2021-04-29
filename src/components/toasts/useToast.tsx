10bae9683c0b4a8b1490bba72034655326dab0d6
Riley Miller <rmllr000@gmail.com>
2021-04-28 19:46:18 -0600

    feat: i like the toasts

---
+++ src/components/toasts/useToast.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import { Popper } from '@workday/canvas-kit-react-popup';
+import Toast from '@workday/canvas-kit-react-toast';
+import { exclamationCircleIcon } from '@workday/canvas-system-icons-web';
+import { colors as canvasColors } from '@workday/canvas-kit-react-core';
+import { colors } from '../../styles/colors';
+import { css } from '@emotion/react';
+type ToastManagerProps = {
+  containerRef: Element
+};
+
+type ToastProps = {
+  message: string
+  actionText?: string
+  handleAction?: () => void
+  handleToastClose: () => void
+};
+const SuccessToast = (props: ToastProps) => (
+  <Toast
+    actionText={props.actionText} onActionClick={props.handleAction}
+    onClose={props.handleToastClose}
+  >
+    {props.message}
+  </Toast>
+);
+
+const ErrorToast = (props: ToastProps) => (
+  <Toast
+    actionText={props.actionText} iconColor={canvasColors.cinnamon500} icon={exclamationCircleIcon}
+    onActionClick={props.handleAction}
+    onClose={props.handleToastClose}
+  >
+    {props.message}
+  </Toast>
+);
+
+type ToastType = 'success' | 'error';
+export type ThrowToastProps = { message: string, actionText?: string, type?: ToastType };
+export const useToast = (
+  { containerRef }:
+    ToastManagerProps) => {
+  const [open, setToastOpen] = React.useState(false);
+  const [message, setMessage] = React.useState('');
+  const [actionText, setActionText] = React.useState('');
+  const [type, setType] = React.useState<'success' | 'error'>('success');
+
+  const reloadWindow = () => window.location.reload();
+
+  const handleToastClose = () => {
+    setToastOpen(false);
+  };
+
+  const throwToast = ({ message, actionText, type = 'success' }: ThrowToastProps) => {
+    setMessage(message);
+    setActionText(actionText ?? '');
+    setType(type);
+    setToastOpen(true);
+  };
+
+  const isSuccess = type === 'success';
+
+  const ToastManager = () => (
+    <div>
+      <Popper placement="top" open={open} anchorElement={containerRef} >
+        {
+          isSuccess ?
+            <SuccessToast
+              message={message}
+              actionText={actionText}
+              handleToastClose={handleToastClose}
+              handleAction={reloadWindow}
+            /> :
+            <ErrorToast
+              message={message}
+              actionText={actionText}
+              handleToastClose={handleToastClose}
+              handleAction={reloadWindow}
+            />
+        }
+      </Popper>
+    </div>
+  );
+
+  return { throwToast, ToastManager };
+};
