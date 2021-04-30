147be67bea6f2bddddac3734e9f52fede3e0e77b
Riley Miller <rmllr000@gmail.com>
2021-04-30 16:41:10 -0600

    refac: more robust bg color

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { colors } from '../styles/colors';
 
 interface WrapperProps {
   className?: string;
@@ -15,5 +16,6 @@ const StyledWrapper = styled.div`
   justify-content: start;
   position: relative;
   min-height: 100vh;
+  background-color: ${colors.royalty.blue};
 `;
 
